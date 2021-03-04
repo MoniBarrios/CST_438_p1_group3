796460c95d99ae3e0241eef41424b86cd9780a11
Anthony Perez <54605746+anthonyperez992@users.noreply.github.com>
2021-03-04 13:46:33 -0800

    close to completion with the wishlist page and edit item modal. just needs database integration

--- app/routes.py
+++ app/routes.py
@@ -23,7 +23,43 @@ app = Flask(__name__)
 
 @app.route('/wishlist', methods = ['GET', 'POST'])
 def wishlist():
-    return render_template('listpage.html')
+
+    items = [
+        {
+            'id': '1',
+            'image': 'https://i.pinimg.com/originals/d3/c4/2a/d3c42a5fafa640f90c4c3746f9fb2c22.jpg',
+            'name': 'mountains',
+            'description': 'beautiful mountains and lake of who knows where',
+            'links': 'google.com'
+        },
+        {
+            'id': '2',
+            'image': 'https://cdn1.matadornetwork.com/blogs/1/2019/10/seljalandsfoss-most-instagrammed-waterfalls-world-1200x855.jpg',
+            'name': 'waterfall',
+            'description': 'beautiful waterfall of unknown area',
+            'links': 'amazon.com'
+        }
+    ]
+
+    return render_template('listpage.html', wishlist = items)
+
+# @app.route('/getWishlist', methods = ['GET', 'POST'])
+# def getWishlist();
+
+#     return wishlist;
+
+@app.route('/edit_item/<item_id>', methods = ['GET', 'POST'])
+def edit_item(item_id):
+
+    item = {
+            'id': '1',
+            'image': 'https://i.pinimg.com/originals/d3/c4/2a/d3c42a5fafa640f90c4c3746f9fb2c22.jpg',
+            'name': 'mountains',
+            'description': 'beautiful mountains and lake of who knows where',
+            'links': 'google.com'
+        }
+
+    return item
 
 
 if __name__ == '__main__':
